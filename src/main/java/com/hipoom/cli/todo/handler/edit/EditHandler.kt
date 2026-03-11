184318f0bf86ff946cdf561e641b01b68d8a27c2
mzdxl@qq.com <>
2026-03-11 23:34:21 +0800

    修复批量编辑的问题

--- src/main/java/com/hipoom/cli/todo/handler/edit/EditHandler.kt
+++ src/main/java/com/hipoom/cli/todo/handler/edit/EditHandler.kt
@@ -13,7 +13,9 @@ import com.hipoom.cli.todo.handler.show.ShowOneItemDetail
 import com.hipoom.cli.todo.handler.show.show
 import com.hipoom.cli.todo.handler.textmapping.persistent.TextMappingStorage
 import com.hipoom.cli.todo.itemDao
+import com.hipoom.cli.todo.printError
 import com.hipoom.cli.todo.printLine
+import com.hipoom.cli.todo.utils.parseIds
 import com.hipoom.cli.workspace.WorkspaceContext
 import org.apache.commons.cli.CommandLine
 import org.apache.commons.cli.Options
@@ -97,89 +99,91 @@ class EditHandler : ApacheCliOptionHandler() {
     }
 
     private fun editWithCliMode(workspace: WorkspaceContext, commandLine: CommandLine) {
-        val id = commandLine.getOptionValue("i")?.toIntOrNull()
-        if (id == null) {
-            printLine("无法解析 id.")
+        val (operators, target) = commandLine.getOptionValue("i").parseIds()
+        if (operators.isEmpty()) {
+            printError("无法解析 id.")
             return
         }
 
-        workspace.itemDao().useItem(
-            id = id,
-            ifNotFound = {
-                printLine("没有找到 id = $id 的事项。")
-                printLine("")
-            },
-            onFound = { item ->
-                // 如果直接指定了带修改项
-                val newContent = commandLine.getOptionValue("c")
-                val newOwner = commandLine.getOptionValue("o")
-                val newDeadline = commandLine.getOptionValue("d")
-                val newLabel = commandLine.getOptionValue("l")
+        operators.forEach { id ->
+            workspace.itemDao().useItem(
+                id = id,
+                ifNotFound = {
+                    printError("没有找到 id = $id 的事项。")
+                    printLine()
+                },
+                onFound = { item ->
+                    // 如果直接指定了带修改项
+                    val newContent = commandLine.getOptionValue("c")
+                    val newOwner = commandLine.getOptionValue("o")
+                    val newDeadline = commandLine.getOptionValue("d")
+                    val newLabel = commandLine.getOptionValue("l")
 
-                if (newContent != null || newOwner != null || newDeadline != null || newLabel != null) {
-                    if (newContent != null) {
-                        item.content = TextMappingStorage.applyMappings(app, newContent)
-                    }
-                    if (newOwner != null) {
-                        item.owner = newOwner
-                    }
-                    if (newDeadline != null) {
-                        item.deadline = DeadLineEditor.parseTimestamp(newDeadline)
-                    }
-                    if (newLabel != null) {
-                        item.labels = newLabel.split(",").map { it.trim() }.toMutableList()
-                    }
-                } else {
-                    while(true) {
-                        printLine("输入想编辑的项目:")
-                        printLine("1: 内容")
-                        printLine("2: 负责人")
-                        printLine("3: 截止时间")
-                        printLine("exit: 退出编辑")
-                        printLine("> ", false)
-                        val index = readln().trim()
-                        when(index) {
-                            "1" -> {
-                                printLine("当前内容是:")
-                                printLine(item.content)
+                    if (newContent != null || newOwner != null || newDeadline != null || newLabel != null) {
+                        if (newContent != null) {
+                            item.content = TextMappingStorage.applyMappings(app, newContent)
+                        }
+                        if (newOwner != null) {
+                            item.owner = newOwner
+                        }
+                        if (newDeadline != null) {
+                            item.deadline = DeadLineEditor.parseTimestamp(newDeadline)
+                        }
+                        if (newLabel != null) {
+                            item.labels = newLabel.split(",").map { it.trim() }.toMutableList()
+                        }
+                    } else {
+                        while(true) {
+                            printLine("输入想编辑的项目:")
+                            printLine("1: 内容")
+                            printLine("2: 负责人")
+                            printLine("3: 截止时间")
+                            printLine("exit: 退出编辑")
+                            printLine("> ", false)
+                            val index = readln().trim()
+                            when(index) {
+                                "1" -> {
+                                    printLine("当前内容是:")
+                                    printLine(item.content)
 
-                                // 如果 JFrame 窗口可用，打开 JFrame 窗口
-                                if (Configs.window.isEnable) {
-                                    printLine("请在新窗口中输入新内容(如果没有出现新的窗口，请使用 edit --disable-window 关闭窗口模式)。")
-                                    val newContent2 = TextEditor.edit(
-                                        input = item.content ?: "",
-                                        prompt = "请输入新内容:"
-                                    )
-                                    item.content = newContent2
+                                    // 如果 JFrame 窗口可用，打开 JFrame 窗口
+                                    if (Configs.window.isEnable) {
+                                        printLine("请在新窗口中输入新内容(如果没有出现新的窗口，请使用 edit --disable-window 关闭窗口模式)。")
+                                        val newContent2 = TextEditor.edit(
+                                            input = item.content ?: "",
+                                            prompt = "请输入新内容:"
+                                        )
+                                        item.content = newContent2
+                                    }
+                                    // 否则，在命令行窗口中输入
+                                    else {
+                                        printLine("请输入新内容:")
+                                        item.content = TextMappingStorage.applyMappings(app, readln().trim())
+                                    }
                                 }
-                                // 否则，在命令行窗口中输入
-                                else {
-                                    printLine("请输入新内容:")
-                                    item.content = TextMappingStorage.applyMappings(app, readln().trim())
+                                "2" -> {
+                                    printLine("当前负责人是:")
+                                    printLine(item.owner)
+                                    printLine("请输入新负责人:")
+                                    item.owner = readln().trim()
                                 }
-                            }
-                            "2" -> {
-                                printLine("当前负责人是:")
-                                printLine(item.owner)
-                                printLine("请输入新负责人:")
-                                item.owner = readln().trim()
-                            }
-                            "3" -> {
-                                val newDeadLine = DeadLineEditor.edit()
-                                if (newDeadLine != null) {
-                                    item.deadline = newDeadLine
+                                "3" -> {
+                                    val newDeadLine = DeadLineEditor.edit()
+                                    if (newDeadLine != null) {
+                                        item.deadline = newDeadLine
+                                    }
                                 }
-                            }
-                            "exit" -> {
-                                break
+                                "exit" -> {
+                                    break
+                                }
                             }
                         }
                     }
-                }
 
-                showOne(item)
-            }
-        )
+                    showOne(item)
+                }
+            )
+        }
     }
 
     private fun showOne(item: Item?) {
